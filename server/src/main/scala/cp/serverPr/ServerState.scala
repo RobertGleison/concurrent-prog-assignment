2ed71d693cfb5d64e7f4a63a91702110668e3395
Robert Gleison <robertgleison12@gmail.com>
2025-05-28 22:05:05 +0100

    feat: increase the counter atomically

--- server/src/main/scala/cp/serverPr/ServerState.scala
+++ server/src/main/scala/cp/serverPr/ServerState.scala
@@ -1,9 +1,13 @@
 package cp.serverPr
+import java.util.concurrent.atomic.AtomicInteger
 
 class ServerState() {
   // TODO: extend the state of the server
+  // Use AtomicInteger for thread-safe counter operations
+  private val _counter = new AtomicInteger(0)
 
-  var counter = 0;
+  def counter: Int = _counter.get()
+  def incrementCounter(): Int = _counter.incrementAndGet()
 
   def toHtml: String =
     s"<p><strong>counter:</strong> $counter</p>"
